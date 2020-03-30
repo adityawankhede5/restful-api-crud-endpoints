0d7bb57210139fc858aec061a3ad844b32d06d0d
adityawankhede5 <adityawankhede555@gmail.com>
2020-03-30 18:17:52 +0530

    Production Build

---
+++ build/precache-manifest.46f4a9d514cf799296240447505f84b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d01f2dff73f62576185d6abdcb0b404",
+    "url": "/restful-api-crud-endpoints/index.html"
+  },
+  {
+    "revision": "b0caef07871eafbd812a",
+    "url": "/restful-api-crud-endpoints/static/css/main.3c9e5021.chunk.css"
+  },
+  {
+    "revision": "0a3efd23e51350cce653",
+    "url": "/restful-api-crud-endpoints/static/js/2.a74eb657.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/restful-api-crud-endpoints/static/js/2.a74eb657.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0caef07871eafbd812a",
+    "url": "/restful-api-crud-endpoints/static/js/main.041f4e67.chunk.js"
+  },
+  {
+    "revision": "a68af9ea8dfb351f371f",
+    "url": "/restful-api-crud-endpoints/static/js/runtime-main.637db049.js"
+  }
+]);
